6f900716dfc6bf98ba0f60f80c7a1b93491a462e
jianfen1 <jianfen1@staff.weibo.com>
2018-12-20 17:08:31 +0800

    ff

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-
+#include "broc_out/kuihuar/up/proto/person.pb.h"
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World, and hi Broc!\n";
-    return 0;
-}
-#include <stdio.h>
-#include "broc_out/kuihuar/up/proto/person.pb.h"
-int main()
-{
-	Person person;
+    Person person;
 	person.set_name("broc");
     person.set_id(1234);
     person.set_email("broc@baidu.com");
-    printf('%s', "Hello, World, and hi Broc!\n");
+    std::cout << "Hello, World, and hi Broc!\n";
     return 0;
 }
