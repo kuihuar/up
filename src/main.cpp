73b56a03ead43c95737b2ae535d7c645588118bb
jianfen1 <jianfen1@staff.weibo.com>
2018-12-20 16:43:41 +0800

    fadfdsa

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,14 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World, and hi Broc!\n";
     return 0;
 }
+#include <stdio.h>
+#include "broc_out/et/tools/app/proto/Person.pb.h"
+int main()
+{
+	Person person;
+	person.set_name("broc");
+    person.set_id(1234);
+    person.set_email("broc@baidu.com");
+    printf('%s', "Hello, World, and hi Broc!\n");
+    return 0;
+}
