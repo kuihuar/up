3c94a818bd44954619f9745055f9396b4d228725
jianfen1 <jianfen1@staff.weibo.com>
2019-01-02 17:38:30 +0800

    2109

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include "broc_out/kuihuar/up/proto/person.pb.h"
 int main(int argc, const char * argv[]) {
     // insert code here...
-    Person person;
-	person.set_name("broc");
-    person.set_id(1234);
-    person.set_email("broc@baidu.com");
     std::cout << "Hello, World, and hi Broc!\n";
+    std::cout << "Hello, World, hello 2019!\n";
     return 0;
 }
