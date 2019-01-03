5fd263338d2253f4473cb561c49081b0b61d1f09
jianfen1 <jianfen1@staff.weibo.com>
2019-01-03 20:45:49 +0800

    add BROC file

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World, and hi Broc!\n";
-    std::cout << "Hello, World, hello 2019!\n";
+    //std::cout << "Hello, World, hello 2019!\n";
     return 0;
 }
